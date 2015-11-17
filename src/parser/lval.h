b52ab65fe0fc3333a556ac1c3ef1240617744c38
Dillon Daudert <dillongdaudert@gmail.com>
2015-11-17 11:00:30 -0500

    Began work on yylval

---
+++ src/parser/lval.h
@@ -0,0 +1,10 @@
+/******************* This header contains the yylval struct *********************
+ *                   Dillon Daudert 11/17/2015                                  *
+ *******************************************************************************/
+
+struct lval {
+    char **names;
+    int count;
+    int size;
+    
+}
