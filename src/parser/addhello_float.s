439baf5e4774bf6c8ee455f69a915fe36f42de97
Dillon Daudert <dillongdaudert@gmail.com>
2015-12-03 22:43:57 -0500

    Checked that integer addition works with local variable declarations.

--- src/parser/addhello_float.s
+++ src/parser/addhello_float.s
@@ -4,9 +4,7 @@
 .int_rformat: .string "%d"
 .flt_wformat: .string "%f\n"
 .flt_rformat: .string "%f"
-	.comm _gp, 16, 4
-
-.string_const0: .string "Hello, world"
+	.comm _gp, 12, 4
 
 	.globl main
 	.type main @function
@@ -15,46 +13,37 @@ main:	nop
 	pushq	%rbp
 	movq	%rsp, %rbp
 	subq	$8, %rsp
-	movl	$.string_const0, %ebx
-	movl	%ebx, %esi
-	movl	$0, %eax
-	movl	$.str_wformat, %edi
-	call	printf              
-	movl	$10, %eax
-	movl	$20, %ebx
-	addl	%eax, %ebx
-	movl	%ebx, %esi
-	movl	$0, %eax
-	movl	$.int_wformat, %edi
-	call	printf
-	movq	$_gp, %rax
-	addq	$0, %rax
-	movl	$1, %ebx
+	movq	%rbp, %rax
+	subq	$4, %rax
+	movl	$6, %ebx
 	movl	%ebx, (%rax)
 	movq	$_gp, %rax
-	addq	$8, %rax
+	addq	$0, %rax
 	movl	$3, %ebx
 	movl	%ebx, (%rax)
 	movq	$_gp, %rax
-	addq	$12, %rax
+	addq	$4, %rax
 	movl	$4, %ebx
 	movl	%ebx, (%rax)
 	movq	$_gp, %rax
-	addq	$4, %rax
+	addq	$8, %rax
 	movq	$_gp, %rbx
 	addq	$0, %rbx
 	movl	(%rbx), %ecx
 	movq	$_gp, %rbx
-	addq	$8, %rbx
+	addq	$4, %rbx
 	movl	(%rbx), %edx
 	addl	%ecx, %edx
-	movq	$_gp, %rbx
-	addq	$12, %rbx
-	movl	(%rbx), %ecx
-	addl	%edx, %ecx
-	movl	%ecx, (%rax)
+	movl	%edx, (%rax)
 	movq	$_gp, %rax
-	addq	$4, %rax
+	addq	$8, %rax
+	movl	(%rax), %ebx
+	movl	%ebx, %esi
+	movl	$0, %eax
+	movl	$.int_wformat, %edi
+	call	printf
+	movq	%rbp, %rax
+	subq	$4, %rax
 	movl	(%rax), %ebx
 	movl	%ebx, %esi
 	movl	$0, %eax
